87afa7b65c9320dfc0c8dfbe6eeee493d2827b2b
Leonardo.Selmini <selminileonardo@gmail.com>
2021-11-19 17:45:57 +0100

    JS array of objects

---
+++ script.js
@@ -0,0 +1,54 @@
+const team = [
+  { name: 'Wayne',
+    lastname: 'Barnett',
+    position: 'Founder & CEO',
+    photo: 'img/walter-gordon-office-manager.jpg'
+  },
+  { name: 'Angela',
+    lastname: 'Carrol',
+    position: 'Chief Editor',
+    photo: 'img/angela-caroll-chief-editor.jpg'
+  },
+  { name: 'Walter',
+    lastname: 'Gordon',
+    position: 'Office Manager',
+    photo: 'img/walter-gordon-office-manager.jpg'
+  },
+  { name: 'Angela',
+    lastname: 'Lopez',
+    position: 'Social Media Menager',
+    photo: 'img/angela-lopez-social-media-manager.jpg'
+  },
+  { name: 'Scott',
+    lastname: 'Estrada',
+    position: 'Developer',
+    photo: 'img/scott-estrada-developer.jpg'
+  },
+  { name: 'Barbara',
+    lastname: 'Ramos',
+    position: 'Graphic Designer',
+    photo: 'img/barbara-ramos-graphic-designer.jpg'
+  },
+  { name: 'Oluwatobiloba',
+    lastname: 'Ray',
+    position: 'New Team Member',
+    photo: 'img/new-team-member-01.jpg'
+  },
+  { name: 'Dalton',
+    lastname: 'Rasmussen',
+    position: 'New Team Member',
+    photo: 'img/new-team-member-02.jpg'
+  },
+  { name: 'Arjun',
+    lastname: 'Cohigikawa',
+    position: 'New Team Member',
+    photo: 'img/new-team-member-03.jpg'
+  },
+  { name: 'Mitchell',
+    lastname: 'Fuller',
+    position: 'New Team Member',
+    photo: 'img/new-team-member-04.jpg'
+  }
+]
+
+
